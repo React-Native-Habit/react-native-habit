1c12e5fec6c4c17f7f7461a59eb9a5f0a9f339a4
LeeYoonjie <ratte00@naver.com>
2024-12-10 22:38:47 +0900

    FEAT: 초기세팅

--- App.js
+++ App.js
@@ -1,34 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import App from './src/App';
 
-export default function App() {
-  useEffect(() => {
-    async function hideSplashScreen() {
-      try {
-        await SplashScreen.preventAutoHideAsync(); 
-        await SplashScreen.hideAsync(); 
-      } catch (e) {
-        console.error(e);
-      }
-    }
-    hideSplashScreen();
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to the App!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
